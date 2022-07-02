7bef5b35da94f141006f2ce46bdd6deed5c436e3
rajeshmn47 <rajeshmn47@gmail.com>
2022-07-02 06:11:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-import { validate } from './validation';
 
 function App() {
   const [users,setUsers]=useState()
@@ -22,21 +21,41 @@ setTimeout(() => {
 
 }, 4000);
   },[])
-  useEffect(()=>{
-validate (values,errors,setErrors)
-console.log(errors,'yt')
-  },[values])
+  const validate=(fieldValues=values)=>{
+    console.log(fieldValues,'rajeshuyttrwe')
+    let temp = { ...errors }
+    if ('userId' in fieldValues)
+        temp.fullName = fieldValues?.userId ? "" : "This field is required."
+    if ('title' in fieldValues)
+        temp.title = fieldValues.title.length>0 ? "" : "Title cannot be empty."
+    if ('body' in fieldValues)
+        temp.body = fieldValues.body.length > 0? "" : "Body cannot be empty."
+    setErrors({
+        ...temp
+    })
+    console.log(temp)
+    if (fieldValues == values)
+        return Object.values(temp).every(x => x == "")
+}
+ 
 const handlechange=(e)=>{
+  console.log('tyui',e.target.value,e.target.name)
   var name=e.target.name
    var value=e.target.value
 setValues({...values,[name]:value})
+validate ({[name]:value,values,errors,setErrors})
 }
 const handlesubmit=(e)=>{
 e.preventDefault()
 if(validate()){
   console.log('ok bro')
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: "post", headers: {'Content-Type': 'application/json'}, 
+    body: values,
+})
 }
 else{
+  alert('fill all the fields')
   console.log('sorry bro')
 }
 }
@@ -46,22 +65,22 @@ else{
   <form  onSubmit={handlesubmit} className='form'>
     <label for='user'>choose the user
     </label>
-    <select name='user' id='user' className='select' value={values.userId} onChange={handlechange}>
-    <option value='santhosh'>choose the user</option>
+    <select name='userId' id='userId' className='select' value={values?.userId&&values.userId} onChange={handlechange}>
+    <option value={values?.username?values.username:'choose the user'}>{values?.username?values.username:'choose the user'}</option>
       {users?.map((a)=><>
 
-        <option value='santhosh'>{a.username}</option>
+        <option value={a}>{a.username}</option>
       </>)}
     </select>
-    {errors?.userId&&errors.userId}
+    {errors?.username&&errors.username}
     <label for='title'>title
     </label>
-    <input type='text' alt='' name='title' className='inputtext' value={values.title} onChange={handlechange}/>
-    {errors?.body&&errors.body}
+    <input type='text' alt='' name='title' className='inputtext' value={values?.title&&values.title} onChange={handlechange}/>
+    {errors?.title&&errors.title} 
     <label for='body'>body
     </label>
-    <input type='text' alt='' name='body' className='inputtext' value={values.body} onChange={handlechange}/>
-    {errors?.title&&errors.title}
+    <input type='text' alt='' name='body' className='inputtext' value={values?.body&&values.body} onChange={handlechange}/>
+ {errors?.body&&errors.body}
     <input type='submit' className='submitbtn'/>
     </form>
    </div>
